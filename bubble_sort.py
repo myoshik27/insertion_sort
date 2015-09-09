280c9dad0b4ac396d7b9ed4900926bd7ee4472dd
Matt Yoshihara <mkyoshihara@gmail.com>
2015-09-09 14:50:31 -0700

    added timestamps

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,4 +1,7 @@
+import time
+
 def bubble_sort(array):
+	start_time = time.time()
 	length = len(array)
 	for value in array:
 		for index in range(0,length-1):
@@ -9,10 +12,20 @@ def bubble_sort(array):
 				value3 = value1
 				array[index] = value2
 				array[index + 1] = value3
+	end_time = time.time()
+	duration = end_time - start_time
+	print duration
 	return array
+
+
 x = []
 for count in range(1,101):
 	from random import randint
-	random_number = randint(0,100)
+	random_number = randint(0,100000)
 	x.append(random_number)
-ubble_sort(x)
+
+
+y = bubble_sort(x)
+print y 
+
+# optimize with while loop and counter
